7b01cb0cae23c472727d217cf678e46d07723452
McDoughnuts <mcdoughnuts@tryprncp.McDoughnuts>
2024-03-29 12:02:19 +0800

    Remove notify

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -467,9 +467,9 @@ return {
             require("trouble").previous({ skip_groups = true, jump = true })
           else
             local ok, err = pcall(vim.cmd.cprev)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
+            -- if not ok then
+            --   vim.notify(err, vim.log.levels.ERROR)
+            -- end
           end
         end,
         desc = "Previous Trouble/Quickfix Item",
@@ -481,9 +481,9 @@ return {
             require("trouble").next({ skip_groups = true, jump = true })
           else
             local ok, err = pcall(vim.cmd.cnext)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
+            -- if not ok then
+            --   vim.notify(err, vim.log.levels.ERROR)
+            -- end
           end
         end,
         desc = "Next Trouble/Quickfix Item",
